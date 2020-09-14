8e6ab4946d3ada7337635b1d829abc5eafc6e92b
1353391279 <1353391279@qq.com>
2020-09-14 15:41:39 +0800

    vue模板

---
+++ vue.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  devServer: {
+    open: true, //自动打开页面
+    proxy: {
+      '/': {
+        target: 'http://127.0.0.1/', //后台接口
+        ws: true, //是否跨域
+        changeOrigin: true,
+        pathRewrite: {
+          '^/':'/'
+        }
+      }
+    }
+  },
+  pages: { //多页面开发
+    index: {
+      entry: 'src/pages/index/main.js',
+      template: 'src/pages/index/index.html',
+      filename: 'index.html',
+      title: 'Index Page',
+    },
+    admin: {
+      entry: 'src/pages/admin/admin.js',
+      template: 'src/pages/admin/admin.html',
+      filename: 'admin.html',
+      title: 'Admin Page',
+    },
+    //   subpage: 'src/subpage/main.js'
+  },
+  publicPath: './' //打包文件路径
+}
